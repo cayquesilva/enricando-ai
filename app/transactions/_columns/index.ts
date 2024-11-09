f924711617ec439c9d5bd5fbbafaf5738ec694c2
Cayque Silva <cayque.ti@gmail.com>
2024-11-09 15:09:30 -0300

    feat: adição de dataTable com shadcn e reacttable - client component importado em server component

---
+++ app/transactions/_columns/index.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];

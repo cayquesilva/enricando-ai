0a123a3841af675056242dabfc73ed510b63737f
Cayque Silva <cayque.ti@gmail.com>
2024-11-09 14:49:25 -0300

    chore: adicionando busca a banco em server component transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,16 @@
-const TransactionsPage = () => {
-    return ( <h1>Transactions page</h1> );
-}
- 
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  //acessar as transações, pois tudo é server component com o uso do approuter do next
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name}</div>
+      ))}
+    </div>
+  );
+};
+
 export default TransactionsPage;

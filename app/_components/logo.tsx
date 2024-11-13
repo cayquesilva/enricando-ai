862d1e18d80b880c94d1c98e774c6ea839354a2f
Cayque Silva <cayque.ti@gmail.com>
2024-11-13 10:47:55 -0300

    feat: adição de novo logo para aplicação

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const LogoIcon = () => {
   return (
     <div className="flex items-center gap-2">
-      <Image src="logo.svg" alt="Enrica Ai" width={39} height={39} />
+      <Image src="/logo.png" alt="Enrica Ai" width={62} height={62} />
       <h3 className="text-2xl font-bold text-white">Enrica.AI</h3>
     </div>
   );
